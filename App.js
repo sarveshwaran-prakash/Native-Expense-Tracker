a0cc112db9d1c5cceebc582171f7723dab5784db
sarveshwaran <sarveshwaranvp@gmail.com>
2024-04-11 11:11:41 +0530

    add navigation

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
 import AddTaskScreen from "./screens/AddTaskScreen";
 import ViewTasksScreen from "./screens/ViewTasksScreen";
+import { Text } from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome"; // for FontAwesome icons
 
 const Tab = createMaterialBottomTabNavigator();
 
@@ -10,8 +12,26 @@ export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name="Add Task" component={AddTaskScreen} />
-        <Tab.Screen name="View Tasks" component={ViewTasksScreen} />
+        <Tab.Screen
+          name="Add Task"
+          component={AddTaskScreen}
+          options={{
+            tabBarLabel: <Text>ADD TASKS</Text>,
+            tabBarIcon: ({ color }) => (
+              <Icon name="plus" color={color} size={26} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="View Tasks"
+          component={ViewTasksScreen}
+          options={{
+            tabBarLabel: <Text>VIEW TASKS</Text>,
+            tabBarIcon: ({ color }) => (
+              <Icon name="list" color={color} size={26} />
+            ),
+          }}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );
